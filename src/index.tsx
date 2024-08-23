ff022971f5caf286c771298920764d40dcc94dd3
Ashutosh Singh <ashutosh_singh@intuit.com>
2024-08-22 22:17:35 -0700

    add topbar action menu  ext

--- src/index.tsx
+++ src/index.tsx
@@ -15,59 +15,19 @@ export interface ExtensionProps {
 
 
 export const ActionExtension: React.FC<ExtensionProps> = ({ application, openFlyout }) => {
-
     return (
-        application.metadata?.labels && application.metadata.labels['application.environmentLabelKey'] === "prd" ? (
             <>
-                <button onClick={openFlyout} style={{ marginRight: '2px' }} className="argo-button argo-button--base">
-                    <span style={{ marginLeft: '-5px', marginRight: '5px' }} className="show-for-large"> Permissions</span>
-                </button>
+             permissions
             </>
-        ):<></>
     );
 };
 
-
-export const StatusExtension: React.FC<ExtensionProps> = ({ application, openFlyout }) => {
-
-
-    const data =  JSON.parse(localStorage.getItem(application.metadata.name)) ;
-    const [refresh, setRefresh] = useState(true);
-
-    useEffect(() => {
-        if (!data) return;
-
-        const expiryTime = moment(data.expiryTime);
-        const diffInSeconds = expiryTime.diff(moment(), 'seconds');
-
-        if (diffInSeconds <= 0) {
-            console.log("Time has expired");
-            localStorage.removeItem(application.metadata?.name);
-            return;
-        }
-
-        const timeoutId = setTimeout(() => {
-            console.log("Time has expired");
-            localStorage.removeItem(application.metadata?.name);
-            setRefresh(false)
-        }, diffInSeconds * 1000); // Convert seconds to milliseconds
-
-        return () => clearTimeout(timeoutId); // Cleanup the timeout on component unmount
-    }, [data]);
-    return  data && data.expiryTime ? (
-        <div key={data?.name}>
-            Devops user can perform the prod changes until {data.currentTime}
-        </div>
-    ) : (
-        <div style={{ fontWeight: 500, color: 'green', textAlign: 'center' }}>READ ONLY</div>
-    );
-};
 
 interface FlyoutProps {
     application: any;
 }
 
-export const ActionFlyout = ({ application }: FlyoutProps) => {
+export const ActionMenuFlyout = ({ application }: FlyoutProps) => {
     const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
     const expiryTime = moment().add(1,'minute');
     const mockData = { expiryTime: expiryTime, currentTime: currentTime, subtitle: "Elevation Granted", userMsg: "Application changes can perform until ...." };
@@ -113,10 +73,13 @@ export const ActionFlyout = ({ application }: FlyoutProps) => {
     return <></>;
 };
 export const toolbarComponent = ActionExtension;
-export const statusComponent = StatusExtension;
 
-export const flyout = ActionFlyout;
+export const flyout = ActionMenuFlyout;
 
+const showDeploy = (application: any) => {
+    console.log(application.metadata?.labels && application.metadata.labels['application.environmentLabelKey'] === "prd");
+    return application.metadata?.labels && application.metadata.labels['application.environmentLabelKey'] === "prd";
+}
 // Register the component extension in ArgoCD
-(window as any)?.extensionsAPI?.registerTopBarExtension(toolbarComponent, TITLE, ID, flyout, true, false);
-(window as any)?.extensionsAPI?.registerStatusPanelExtension(statusComponent, TITLE, ID);
+(window as any)?.extensionsAPI?.
+registerTopBarActionMenuExt(toolbarComponent, TITLE, ID,flyout,showDeploy,'', true);
